fd2bbddac3a628d1f4a981f404d7c7eb662db3a9
liao wen chung <wcliao1962@gmail.com>
2022-03-12 18:14:34 +0800

    修改migrations: users改為members; 並執行migrate

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('members', function (Blueprint $table) {
             $table->text('two_factor_secret')
                     ->after('password')
                     ->nullable();
